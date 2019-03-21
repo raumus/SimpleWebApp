5c730e692146dcb71bf97b5b70b0a10059b4e825
Raushan Mustafin <raumus@gmail.com>
2019-03-21 15:15:27 +0000

    Adding Heroku to the test

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -6,7 +6,14 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.toLowerCase().contains("ivan moore")){
+        }
+
+        if (query.toLowerCase().contains("heroku")) {
+            return "Heroku, online";
+        }
+
+
+        else if (query.toLowerCase().contains("ivan moore")){
             return "Ivan Moore is the APE lecturer.";
         }
         return "";
--- src/test/java/com/develogical/app/QueryProcessorTest.java
+++ src/test/java/com/develogical/app/QueryProcessorTest.java
@@ -23,4 +23,15 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void knowsIvan() throws Exception {
+        assertThat(queryProcessor.process("ivan moore"), containsString("lecturer"));
+    }
+
+    @Test
+    public void knowsHeroku() throws Exception {
+        assertThat(queryProcessor.process("heroku"), containsString("online"));
+    }
+
 }
